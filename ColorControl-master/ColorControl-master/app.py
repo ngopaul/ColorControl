91945276735b0033b07437e7ae384fdbe6694623
briancnguyen <briancnguyen@berkeley.edu>
2018-11-03 14:26:45 -0700

    Testing Subprocess

--- ColorControl-master/ColorControl-master/app.py
+++ ColorControl-master/ColorControl-master/app.py
@@ -17,17 +17,6 @@ pins = {
    25 : {'name' : 'Blue', 'state' : GPIO.LOW}
 }
 
-GPIO.setup(18, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-
-frequency = 70
-
-clear_lights()
-
-def convert(color):
-    color['green'] *= 200/255
-    color['blue'] *= 150/255
-    return color
-
 colors = {
     # White (255. 200, 150)
     'white': {'red': 255.0, 'green': 200.0, 'blue': 150.0},
@@ -60,6 +49,13 @@ colors = {
     'navy' : convert({'red': 0.0, 'green': 0.0, 'blue': 128.0}),
 }
 
+GPIO.setup(18, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+
+frequency = 70
+
+clear_lights()
+
+
 # Goal: Web Requests will initiate GPIO methods
 @app.route("/", methods=['GET', 'POST'])
 def main():
@@ -74,7 +70,16 @@ def on(color):
     pi.set_PWM_dutycycle(24, 0)
     pi.set_PWM_dutycycle(25, 0)
     if color in colors:
-        get_lit(colors[color])
+        get_lit(color)
+    return render_template('main.html')
+
+@app.route('/off/', methods=['GET', 'POST'])
+def off():
+    print('Turning off the lights')
+    clear_lights()
+    pi.set_PWM_dutycycle(23, 0)
+    pi.set_PWM_dutycycle(24, 0)
+    pi.set_PWM_dutycycle(25, 0)
     return render_template('main.html')
 
 @app.route('/flash/<color>/<hi_time>/<lo_time>', methods=['GET', 'POST'])
@@ -86,14 +91,10 @@ def flash(color, hi_time, lo_time):
     clear_lights()
     return render_template('main.html')
 
-@app.route('/off/', methods=['GET', 'POST'])
-def off():
-    print('Turning off the lights')
-    clear_lights()
-    pi.set_PWM_dutycycle(23, 0)
-    pi.set_PWM_dutycycle(24, 0)
-    pi.set_PWM_dutycycle(25, 0)
-    return render_template('main.html')
+def convert(color):
+    color['green'] *= 200/255
+    color['blue'] *= 150/255
+    return color
 
 def clear_lights():
     pi.wave_clear()
@@ -109,13 +110,14 @@ def clear_lights():
     pi.set_PWM_dutycycle(24, 0)
     pi.set_PWM_dutycycle(25, 0)
 
-def get_lit(color_dict):
+def get_lit(color):
+    color_dict = colors[color]
     pi.set_PWM_dutycycle(23, color_dict['red'])
     pi.set_PWM_dutycycle(24, color_dict['green'])
     pi.set_PWM_dutycycle(25, color_dict['blue'])
 
 def custom_pwm(pin, duty, length):
-    print(length-2650)
+    print(length - 2650)
     toReturn = [pigpio.pulse(1<<pin, 0, duty*10), pigpio.pulse(0, 1<<pin, 2560 - duty*10)] * max(length - 2560, 1)
     return toReturn
 
--- ColorControl-master/ColorControl-master/features.py
+++ ColorControl-master/ColorControl-master/features.py
@@ -1,11 +1,13 @@
-from app import *
+from . import app
 import time
+import sys
 
-def flash(color_dict):
+def flash(color, hi_time, lo_time):
     while True:
-        get_lit(color_dict)
+        app.get_lit(color_dict)
         time.sleep(hi_time/1000)
-        clear_lights()
+        app.clear_lights()
         time.sleep(lo_time/1000)
 
 if __name__ == '__main__':
+    flash(sys.argv[1], sys.argv[2], sys.argv[3])
