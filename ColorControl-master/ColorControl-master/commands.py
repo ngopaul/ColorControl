74d9627085a721928e0996f6c8508b6a665354ff
briancnguyen <briancnguyen@berkeley.edu>
2018-11-03 14:08:01 -0700

    Modularizing Pt.1

---
+++ ColorControl-master/ColorControl-master/commands.py
@@ -0,0 +1,25 @@
+import time
+
+def clear_lights():
+    pi.wave_clear()
+    pi.set_mode(23, pigpio.OUTPUT)
+    pi.set_mode(24, pigpio.OUTPUT)
+    pi.set_mode(25, pigpio.OUTPUT)
+
+    pi.set_PWM_frequency(23, frequency)
+    pi.set_PWM_frequency(24, frequency)
+    pi.set_PWM_frequency(25, frequency)
+
+    pi.set_PWM_dutycycle(23, 0)
+    pi.set_PWM_dutycycle(24, 0)
+    pi.set_PWM_dutycycle(25, 0)
+
+def get_lit(color_dict):
+    pi.set_PWM_dutycycle(23, color_dict['red'])
+    pi.set_PWM_dutycycle(24, color_dict['green'])
+    pi.set_PWM_dutycycle(25, color_dict['blue'])
+
+def custom_pwm(pin, duty, length):
+    print(length-2650)
+    toReturn = [pigpio.pulse(1<<pin, 0, duty*10), pigpio.pulse(0, 1<<pin, 2560 - duty*10)] * max(length - 2560, 1)
+    return toReturn
---
+++ ColorControl-master/ColorControl-master/features.py
@@ -0,0 +1,14 @@
+from . import commands
+from . import app
+
+def flash(color_dict):
+    while True:
+        commands.get_lit(color_dict)
+        time.sleep(hi_time/1000)
+        commands.clear_lights()
+        time.sleep(lo_time/1000)
+
+
+
+if __name__ == '__main__':
+    
