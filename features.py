7220734fb20f5b06d954bc55ead53e6c70e97b45
Paul Ngo <paulngo123@gmail.com>
2018-11-04 04:41:47 -0800

    found error

--- features.py
+++ features.py
@@ -70,16 +70,13 @@ def multi_breathe(colors, length, lo_time):
     length = float(length)
     lo_time = float(lo_time)
     n = len(colors)
-    print(colors)
+    i = 0
     while True:
         counter = 0
-        i = 0
         while counter < 100:
             counter += 1
             get_lit_more(colors[i], math.sin(math.pi*counter/100))
             time.sleep(length/100000)
-        print(colors[i])
-        print(i, n)
         i = (i + 1) % n
         time.sleep(lo_time/1000)
         
